c86e63e37eb4cab8f14c681ff3b075c2fbd09708
LGY2008 <150691555@qq.com>
2019-04-07 15:53:45 +0800

    Initial commit

---
+++ base/base.py
@@ -0,0 +1,57 @@
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from base.get_logger import GetLogger
+
+
+# 获取log日志器
+log = GetLogger().get_logger()
+
+
+class Base:
+
+    def __init__(self, driver):
+        log.info("[base]: 正在获取初始化driver对象:{}".format(driver))
+        self.driver = driver
+
+    # 查找元素方法 封装
+    def base_find(self, loc,  timeout=30, poll=0.5):
+        log.info("[base]: 正在定位:{} 元素，默认定位超时时间为: {}".format(loc, timeout))
+        # 使用显示等待 查找元素
+        return WebDriverWait(self.driver,
+                             timeout=timeout,
+                             poll_frequency=poll).until(lambda x:x.find_element(*loc))
+
+    # 点击元素 方法封装
+    def base_click(self, loc):
+        log.info("[base]: 正在对:{} 元素实行点击事件".format(loc))
+        self.base_find(loc).click()
+
+    # 输入元素 方法封装
+    def base_input(self, loc, value):
+        # 获取元素
+        el = self.base_find(loc)
+        # 清空
+        log.info("[base]: 正在对:{} 元素实行清空".format(loc))
+        el.clear()
+        # 输入
+        el.send_keys(value)
+
+    # 获取文本信息 方法封装
+    def base_get_text(self, loc):
+        log.info("[base]: 正在获取:{} 元素文本值".format(loc))
+        return self.base_find(loc).text
+
+    # 截图 方法封装
+    def base_get_image(self):
+        log.info("[base]: 断言出错，调用截图")
+        self.driver.get_screenshot_as_file("../image/{}.png".format(time.strftime("%Y_%m_%d %H_%M_%S")))
+
+    # 判断元素是否存在 方法封装
+    def base_element_is_exist(self, loc):
+        try:
+            self.base_find(loc, timeout=2)
+            log.info("[base]: {} 元素查找成功，存在页面".format(loc))
+            return True # 代表元素存在
+        except:
+            log.info("[base]: {} 元素查找失败，不存在当前页面".format(loc))
+            return False # 代表元素不存在
