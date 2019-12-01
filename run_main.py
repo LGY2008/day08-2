d3c53b5126091480636a5da2347028aab3a7fc95
LGY2008 <39271443+LGY2008@users.noreply.github.com>
2019-12-01 18:54:25 +0800

    dd
    sss

--- run_main.py
+++ run_main.py
@@ -9,7 +9,7 @@ from tool.HTMLTestRunner import HTMLTestRunner
 # 在最外侧运行
 suite = unittest.defaultTestLoader.discover("./scripts")
 # 报告生成目录及文件名称
-dir_path = "./report/{}.html".format(time.strftime("%Y_%m_%d %H_%M_%S"))
+dir_path = "./report/report.html"
 # 获取文件流并调用run运行
 with open(dir_path, "wb") as f:
     HTMLTestRunner(stream=f, title="Tpshop商城自动化测试报告", description="操作系统：win7").run(suite)
